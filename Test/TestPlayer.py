82a38f3b8e76a56b4202055ddb10dc37e9cc197c
Netwear7 <59568212+Netwear7@users.noreply.github.com>
2021-05-04 21:57:53 +0200

    Update TestPlayer.py

--- Test/TestPlayer.py
+++ Test/TestPlayer.py
@@ -13,18 +13,16 @@ class TestPlayer(unittest.TestCase):
     def setUp(self):
         self.instance_player = Player("TestPlayer")
 
-    def testAttackDamage_IfGreaterThan100_ExceptionThrown(self):
-        for i in range(1, 100):
-            attack_damage = self.instance_player.random_attack_damage()
-            self.assertLessEqual(attack_damage, 100)
+    def testAttackDamage_IfGreaterThan100(self):
+        attack_damage = self.instance_player.random_attack_damage()
+        self.assertLessEqual(attack_damage, 100)
 
-    def testAttackDamage_IfLessThan1_ExceptionThrown(self):
-        # Test 100 times
-        for i in range(1, 100):
-            attack_damage = self.instance_player.random_attack_damage()
-            self.assertGreaterEqual(attack_damage, 1)
+    def testAttackDamage_IfLessThan1(self):
+        attack_damage = self.instance_player.random_attack_damage()
+        self.assertGreaterEqual(attack_damage, 1)
 
     def testInstanceOfPlayer(self):
+        #wip
         pass
 
     if __name__ == '__main__':
